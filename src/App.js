af0c8fb327a8f5af881105cc00ae8c05f22945ef
jeezzas <mostafahakkou1999@gmail.com>
2022-12-07 02:31:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="header-form">
+          <h2 className="col-span-3">What is Your Roseville Home Worth Now?</h2>
+          <p className="col-span-3">
+            Recent home sales have affected your home’s value! Get your home’s
+            new value estimate instantly for free!
+          </p>
+          <input
+            type="text"
+            name="adress"
+            id="adress"
+            placeholder="Enter your home address"
+            className="col-span-2"
+          />
+          <button type="submit" className="col-span-1">
+            Get value estimate
+            <span class="arrow"></span>
+          </button>
+        </div>
       </header>
+      <div className="container">
+        <h3>Discover the value of your property</h3>
+        <div className="values">
+          <div className="value">
+            <h4>Track your home’s value</h4>
+            <p>
+              Instantly learn your home’s value and how much you could sell for
+              in today’s market.
+            </p>
+          </div>
+          <div className="value">
+            <h4>See local market trends</h4>
+            <p>
+              Stay up to date on market changes and find out how much homes like
+              yours have sold in the past year.
+            </p>
+          </div>
+          <div className="value">
+            <h4>Get a free monthly home report</h4>
+            <p>
+              Receive a monthly email with updates about your home value and how
+              it’s changing.
+            </p>
+          </div>
+        </div>
+        <div className="book"></div>
+      </div>
     </div>
   );
 }
